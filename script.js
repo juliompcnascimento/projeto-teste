5c337a434592bfb33d6e06a33e463b6ba3da5580
juliompcnascimento <138039848+juliompcnascimento@users.noreply.github.com>
2023-11-09 22:08:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
+let url = "https://script.google.com/macros/s/AKfycbww0SLu5_EuH6ELq5BgxkvWT96F1-JaUzvy-IGI4Co9zCP9dOtLKfsV_1osfC4PQi_d/exec?action=getInfo";
+
+const listaClientes = () => {
+    return fetch(url)
+    .then( resposta => {
+    return resposta.json()
+    })
+  }
+
 function testeFetch() {
     alert("Entrou");
 
     // let url = "https://script.google.com/macros/s/AKfycbzgt4c800NjRUJDF-8gnoNbsa8hS-YYD3VqCPxSzNBmRmy1jAGySmXil2cJAGQXU2dbng/exec?action=getInfo";
 
-    let url = "https://script.google.com/macros/s/AKfycbww0SLu5_EuH6ELq5BgxkvWT96F1-JaUzvy-IGI4Co9zCP9dOtLKfsV_1osfC4PQi_d/exec?action=getInfo";
+    
 
     // const url = `https://viacep.com.br/ws/${60325-582}/json/`;
     
-    alert("fetch");
+    listaClientes()
+    .then(data => {
+            alert("ok");
+    })
+}
 
-    fetch("https://google.com.br").then(function(response) {
-            alert("reponse");
-        });
-
-    alert("saiu");
+function funcaoOne() {
+    document.getElementById("parag").innerHTML = "fetch response no";
 }
