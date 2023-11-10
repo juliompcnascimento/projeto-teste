9d2c77dd8a275b76c2de9c21e33670759635856b
juliompcnascimento <138039848+juliompcnascimento@users.noreply.github.com>
2023-11-10 18:10:42 -0300

    update

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
-let url = "https://script.google.com/macros/s/AKfycbww0SLu5_EuH6ELq5BgxkvWT96F1-JaUzvy-IGI4Co9zCP9dOtLKfsV_1osfC4PQi_d/exec?action=getInfo";
+// let url = "https://script.google.com/macros/s/AKfycbww0SLu5_EuH6ELq5BgxkvWT96F1-JaUzvy-IGI4Co9zCP9dOtLKfsV_1osfC4PQi_d/exec?action=getInfo";
 
-const listaClientes = () => {
-    return fetch(url)
-    .then( resposta => {
-    return resposta.json()
-    })
-  }
+let url = "https://script.google.com/macros/s/AKfycbzt7ZeVMgDl7agJSBXD16QiH1vXv7AboqTWzuoBmCYbV8e9Lv52XsJrN4QoFVUu9TwuKA/exec?link=${'https://docs.google.com/spreadsheets/d/1lLdSovGe_9jWfMGg-P8fzEZiaet4VuSLEnw9S1Y6R78/edit'}&pagina=${'pag1'}&celulas={'A1:F'}";
 
-function testeFetch() {
-    alert("Entrou");
-
-    // let url = "https://script.google.com/macros/s/AKfycbzgt4c800NjRUJDF-8gnoNbsa8hS-YYD3VqCPxSzNBmRmy1jAGySmXil2cJAGQXU2dbng/exec?action=getInfo";
-
-    
-
-    // const url = `https://viacep.com.br/ws/${60325-582}/json/`;
-    
-    listaClientes()
-    .then(data => {
-            alert("ok");
-    })
-}
-
-function funcaoOne() {
-    document.getElementById("parag").innerHTML = "fetch response no";
+function fazerRequisicao() {
+    axios.get(url)
+        .then(function (response) {
+        document.getElementById('parag').innerHTML = 'Dados recebidos: ' + JSON.stringify(response.data);
+        })
+        .catch(function (error) {
+        document.getElementById('parag').innerHTML = 'Erro: ' + error.message;
+        });
 }
